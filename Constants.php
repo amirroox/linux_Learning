31667304a4fdbf9b92190ea929d56ddd287efdbf
Amir Roox <amirroox@yahoo.com>
2023-07-03 18:16:24 +0430

    Add Tailwind to Project + config

---
+++ Constants.php
@@ -0,0 +1,7 @@
+<?php
+const TURN_SITE = 0;  /* 0 -> Turn On    , 1 -> Turn Off */
+if (TURN_SITE) exit();
+const MAIN_DIR = __DIR__ . '/';  #Main Directory
+const MAIN_SERVER = "http://localhost:63342/Linux_Learning" . '/';  # Main Server
+const MAIN_GITHUB = "https://github.com/amirroox/Linux_Learning"; # Github Page
+const MAIN_SITE = "https://ro-ox.com";
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-
-echo "Hello";
+<?php include_once "Constants.php" ?>
+<!doctype html>
+<html lang="fa" dir="rtl">
+<head>
+    <?php include MAIN_DIR . "assets/css/MainMeta.php" ?>
+    <title>Linux Learn</title>
+</head>
+<body class="bg-red-500">
+<h1 class="text-2xl font-bold underline ">
+    Hello world!
+</h1>
+</body>
+</html>
